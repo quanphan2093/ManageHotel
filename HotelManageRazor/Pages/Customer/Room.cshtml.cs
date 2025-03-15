ef29d8eda9b426c0cb6780d7c3a6fc3ffb5d26ad
quanphan2k3 <quanpahe170260@fpt.edu.vn>
2025-03-15 21:22:15 +0700

    add fe

---
+++ HotelManageRazor/Pages/Customer/Room.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelManageRazor.Pages.Customer
+{
+    public class RoomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HotelManageRazor/Pages/Customer/Service.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelManageRazor.Pages.Customer
+{
+    public class ServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
